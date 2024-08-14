d5e2450ee3f519e5a2ca6dd579ac4d1af2d221b3
MidoriKami <mi_kami@outlook.com>
2024-08-13 22:18:24 -0700

    Set reasonable override defaults

--- HUDUnlimited/Classes/NodeOverride.cs
+++ HUDUnlimited/Classes/NodeOverride.cs
@@ -11,12 +11,12 @@ public class NodeOverride {
     
     public bool OverrideEnabled;
     
-    public Vector2 Position;
-    public Vector2 Scale;
-    public Vector4 Color;
-    public Vector3 AddColor;
-    public Vector3 MultiplyColor;
-    public bool Visible;
+    public Vector2 Position = Vector2.Zero;
+    public Vector2 Scale = Vector2.One;
+    public Vector4 Color = Vector4.One;
+    public Vector3 AddColor = Vector3.Zero;
+    public Vector3 MultiplyColor = Vector3.One;
+    public bool Visible = true;
     
     public OverrideFlags Flags;
 }
