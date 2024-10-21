c41f7169dafd3e0251b796f09205f29a184cb902
MidoriKami <mi_kami@outlook.com>
2024-10-20 19:07:47 -0700

    Remove unused service

--- HUDUnlimited/Service.cs
+++ HUDUnlimited/Service.cs
@@ -7,7 +7,6 @@ namespace HUDUnlimited;
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 public class Service {
     [PluginService] public static IDalamudPluginInterface PluginInterface { get; set; }
-    [PluginService] public static IFramework Framework { get; set; }
     [PluginService] public static IAddonLifecycle AddonLifecycle { get; set; }
     [PluginService] public static IPluginLog PluginLog { get; set; }
     [PluginService] public static INotificationManager NotificationManager { get; set; }
